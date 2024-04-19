893686ecd9515ebc30e5d9397fbf2b795be5a643
Lukyanau Mikhail <lukcad@gmail.com>
2024-04-19 16:44:40 +0000

    version 1.0.1

--- srv/code/orders-loyalty-logic.js
+++ srv/code/orders-loyalty-logic.js
@@ -1,8 +1,21 @@
 /**
  * 
  * @On(event = { "Action1" }, entity = "NorthwindSrv.Orders")
- * @param {Object} request - User information, tenant-specific CDS model, headers and query parameters
+ * @param {Object} req - User information, tenant-specific CDS model, headers and query parameters
 */
-module.exports = async function(request) {
-	// Your code here
+module.exports = async function(req) {
+    const tx = cds.transaction(req);
+    const orderID = req._params[0].ID; // req.data.ID;
+    const orderDetails = await tx.run(
+        SELECT
+            .from('NorthwindSrv.OrderDetails')
+            .where({ order_ID: orderID })
+    );
+
+    let totalCost = 0;
+    for (const detail of orderDetails) {
+        totalCost += detail.cost;
+    }
+
+    req.info(`The total cost of order ${orderID} is ${totalCost}`);
 }
